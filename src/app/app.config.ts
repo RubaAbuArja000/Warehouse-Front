32dd8ad8dcdea5158a640e3445bcb48115e250e1
RubaAbuArja1 <rabuarja@kaizenplus.com>
2026-05-06 13:11:13 +0300

    Fix library (Primeng) version also Import Aura style

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
+
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MessageService, ConfirmationService } from 'primeng/api';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 import { provideEchartsCore } from 'ngx-echarts';
 
 import { routes } from './routes';
@@ -21,8 +24,15 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
     MessageService,
     ConfirmationService,
-    provideEchartsCore({ echarts: () => import('echarts') }),
+    provideEchartsCore({
+      echarts: () => import('echarts'),
+    }),
   ],
 };
